d1477dc48427f664818b8fcdc31d730a9c56cc4e
etiennea <alexis.etienne@immofix.com>
2020-01-26 23:00:36 +0100

    addDependency

--- server/index.js
+++ server/index.js
@@ -70,16 +70,11 @@ app.post('/contact', (req, res) => {
 
   // create reusable transporter object using the default SMTP transport
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
     host: 'smtp.gmail.com',
     port: 465,
-    secure: 'true',
     auth: {
       user: 'alexisetienne86@gmail.com', // could use generated ethereal user
       pass: 'etienne0290', // could use generated ethereal password
-    },
-    tls: {
-      rejectUnauthorized: false,
     },
   });
 
